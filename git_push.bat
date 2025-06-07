cdda661c6aa6d7d7712349cbed44a512d2534b02
TharunReddy-M123 <kumarreddym9390@gmail.com>
2025-06-07 14:28:49 +0530

    Auto commit from Jenkins

--- git_push.bat
+++ git_push.bat
@@ -1,7 +1,20 @@
-@echo off
+cd D:\CHKSUM_PYTHON_SCRIPT\CHKSUM_CHECKER
+
+:: Checkout the actual branch (main)
+git checkout main
+
+:: Pull latest changes from GitHub
 git pull origin main
+
+:: Stage changes
 git add .
-git commit -m "Auto commit: %date% %time%"
+
+:: Commit changes (only if there are any)
+git diff --cached --quiet || git commit -m "Auto commit from Jenkins"
+
+:: Push changes to GitHub
 git push origin main
+
 echo Git push completed.
+
 pause
